b803be0f0a877177d1e923e14abf0b5ef75f0460
[ascir] <[anjanar876@gmail.com]>
2020-05-04 10:54:48 +0530

    welcomeprop

---
+++ src/App/index.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react';
+import Welcome from "./welcomemsg"
+import './App.css';
+import styled, {css} from 'styled-components'
+
+const MyButton = styled.button `
+display: inline-block;
+border-radius: 3px;
+padding: 0.5rem 0;
+margin: 0.5rem 1rem;
+width: 11rem;
+background: transparent;
+color: palevioletred;
+border: 2px solid palevioletred;
+${props => props.primary && css`
+background: white;
+color: palevioletred;
+`};
+`
+
+const TomatoButton = styled(MyButton)`
+  color: tomato;
+  border: 2px solid tomato;
+`;
+
+
+class App extends Component {
+  render(){     
+  return (
+    <div>
+<Welcome />
+<MyButton>Hello!</MyButton>
+<MyButton primary>Hello again!</MyButton>
+    <TomatoButton>Hi, the name is Bond</TomatoButton>
+    </div>
+  );
+}
+}
+
+export default App;
